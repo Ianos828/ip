5083ea9803d5b4ee21921f035b80b2b4d6b6c458
Ianos828 <kokbowen17@gmail.com>
2026-01-23 16:32:43 +0800

    Add ability to store and display user text

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke!");
         System.out.println("What can I do for you?");
+
         Scanner scanner = new Scanner(System.in);
+        List<String> list = new ArrayList<>();
 
         while (true) {
             String input = scanner.nextLine();
+
             if (input.equals("bye")) {
                 break;
             }
-            System.out.println(input);
+
+            if (input.equals("list")) {
+                printList(list);
+                continue;
+            }
+
+            list.add(input);
+            System.out.printf("added: %s%n", input);
         }
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void printList(List<String> list) {
+        int itemIndex = 1;
+        for (String item : list) {
+            System.out.printf("%d. %s%n", itemIndex, item);
+            itemIndex++;
+        }
     }
 }
