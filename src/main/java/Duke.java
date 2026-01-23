ca35a5b397044184ecc086c882e5efa0ec86b7d8
Ianos828 <kokbowen17@gmail.com>
2026-01-23 16:21:17 +0800

    Improve skeletal version

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke!");
         System.out.println("What can I do for you?");
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println(input);
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
