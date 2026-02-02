1dc56841df67cecbe05572c927b2c92d91df27e3
Ianos828 <kokbowen17@gmail.com>
2026-02-02 21:24:39 +0800

    Add support for task list initialisation via task.txt in chatbot

--- src/main/java/duchess/Duchess.java
+++ src/main/java/duchess/Duchess.java
@@ -4,7 +4,7 @@ import command.Command;
 import exception.InvalidArgumentException;
 import exception.MissingArgumentException;
 import parser.CommandParser;
-import storage.TaskSaver;
+import storage.Storage;
 import task.TaskList;
 
 import java.nio.file.Path;
@@ -17,28 +17,29 @@ import java.util.Scanner;
  */
 public class Duchess {
     private static final Path SAVE_FILE_PATH = Paths.get(".", "data", "tasks.txt");
+
     /**
      * Runs the chatbot.
      *
      * @param args optional startup arguments
      */
     public static void main(String[] args) {
-        TaskSaver saver = new TaskSaver(SAVE_FILE_PATH);
-
-        System.out.println("Hello! I'm Duchess!");
-        System.out.println("What can I do for you?");
+        Storage storage = new Storage(SAVE_FILE_PATH);
 
         boolean shouldTerminate = false;
 
         Scanner scanner = new Scanner(System.in);
-        TaskList list = new TaskList();
+        TaskList tasks = storage.loadTasksFromFile();
+
+        System.out.println("Hello! I'm Duchess!");
+        System.out.println("What can I do for you?");
 
         while (!shouldTerminate) {
             String input = scanner.nextLine();
             Command command = CommandParser.getCommand(input);
 
             try {
-                command.execute(list, saver);
+                command.execute(tasks, storage);
                 shouldTerminate = command.isTerminatingCommand();
             } catch (InvalidArgumentException | MissingArgumentException e) {
                 System.out.println(e.getMessage());
