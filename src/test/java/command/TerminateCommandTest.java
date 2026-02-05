86f0368d986a3021062441fab2c7ff58a2d62fde
Ianos828 <kokbowen17@gmail.com>
2026-02-05 21:03:19 +0800

    Add JUnit test for TerminateCommand

---
+++ src/test/java/command/TerminateCommandTest.java
@@ -0,0 +1,38 @@
+package command;
+
+import duchess.command.CommandType;
+import duchess.command.TerminateCommand;
+import duchess.storage.Storage;
+import duchess.task.TaskList;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TerminateCommandTest {
+    TerminateCommand command;
+    TaskList tasks;
+    Storage storage;
+
+    @BeforeEach
+    public void setUp() {
+        command = new TerminateCommand(CommandType.BYE);
+        tasks = new TaskList();
+        storage = new Storage(Paths.get(".", "data", "tasks.txt"));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        command = null;
+        tasks = null;
+        storage = null;
+    }
+
+    @Test
+    public void testExecute() {
+        assertEquals("Bye. Hope to see you again soon!", command.execute(tasks, storage), "Ends the program");
+    }
+}
