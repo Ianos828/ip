f6a9d58d3f1ec6a445f4577ceb719ac2e6cf9a5c
Ianos828 <kokbowen17@gmail.com>
2026-02-05 21:06:28 +0800

    Add JUnit test for UnknownCommand

---
+++ src/test/java/command/UnknownCommandTest.java
@@ -0,0 +1,40 @@
+package command;
+
+import duchess.command.CommandType;
+import duchess.command.UnknownCommand;
+import duchess.storage.Storage;
+import duchess.task.TaskList;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UnknownCommandTest {
+    UnknownCommand command;
+    TaskList tasks;
+    Storage storage;
+
+    @BeforeEach
+    public void setUp() {
+        command = new UnknownCommand(CommandType.UNKNOWN);
+        tasks = new TaskList();
+        storage = new Storage(Paths.get(".", "data", "tasks.txt"));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        command = null;
+        tasks = null;
+        storage = null;
+    }
+
+    @Test
+    public void testExecute() {
+        assertEquals("I'm sorry, but I don't know what that means :(",
+                command.execute(tasks, storage),
+                "Prints an error message");
+    }
+}
