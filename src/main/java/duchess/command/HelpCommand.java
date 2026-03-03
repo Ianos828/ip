5aba7e946a48dfdf9b7c6e401031946c137dd6fd
Ianos828 <kokbowen17@gmail.com>
2026-03-03 14:51:03 +0800

    Fix checkstyle violations that cause workflow to fail

--- src/main/java/duchess/command/HelpCommand.java
+++ src/main/java/duchess/command/HelpCommand.java
@@ -18,41 +18,29 @@ public class HelpCommand extends Command {
      */
     public String execute(TaskList tasks, Storage storage) {
         return """
-                Hark, attend to the commands at thy disposal:
-                
+                Hark, attend to the commands at thy disposal:\n
                 1. todo NAME
-                - doth forge a task.
-                
+                - doth forge a task.\n
                 2. deadline NAME /by DATE
-                - doth set a task with a term.
-                
+                - doth set a task with a term.\n
                 3. event NAME /from START_DATE /to END_DATE
-                - doth frame a task 'twixt two suns.
-                
+                - doth frame a task 'twixt two suns.\n
                 4. delete INDEX
-                - doth strike down a task by its given count.
-                
+                - doth strike down a task by its given count.\n
                 5. list
-                - doth show forth all tasks in thy ledger.
-                
+                - doth show forth all tasks in thy ledger.\n
                 6. cheer
-                - doth speak a random word of courage.
-                
+                - doth speak a random word of courage.\n
                 7. outstanding DATE
-                - doth reveal tasks yet to be done by the set day.
-                
+                - doth reveal tasks yet to be done by the set day.\n
                 8. find KEYWORD
-                - doth seek tasks akin to the given word.
-                
+                - doth seek tasks akin to the given word.\n
                 9. mark INDEX
-                - doth deem a task done by its count.
-                
+                - doth deem a task done by its count.\n
                 10. unmark INDEX
-                - doth call back a task to be done.
-                
+                - doth call back a task to be done.\n
                 11. bye
-                - doth bid farewell to this program.
-                
+                - doth bid farewell to this program.\n
                 For deeper lore, seek the README.md.""";
     }
 }
